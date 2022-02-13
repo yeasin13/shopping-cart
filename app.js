63536c2a8c3310ac11ad9dd8fa53117f12f306c7
yeasin13 <yeasinarafat8001@gmail.com>
2022-02-13 16:22:08 +0600

    buttonp

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('I am in a separate file')
+document.getElementById('case-plus').addEventListener('click',function(){
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    caseInput.value = caseNumber + 1;
+    console.log(caseNumber);
+})
