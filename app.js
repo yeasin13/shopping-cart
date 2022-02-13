ef5ead9e647628a8551036e0f6fd22179e01f6f8
yeasin13 <yeasinarafat8001@gmail.com>
2022-02-13 16:48:09 +0600

    function-add

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
+    function updateCaseNumber(isIncreasing){
+        const caseInput = document.getElementById('case-number');
+        const caseNumber = caseInput.value;
+        
+        if(isIncreasing){
+            caseInput.value = parseInt(caseNumber) + 1;
+        }
+        else if(caseNumber > 0){
+            caseInput.value = parseInt(caseNumber) - 1;
+        }
+
+        //update case total
+    }
+
 document.getElementById('case-plus').addEventListener('click',function(){
-    const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value = caseNumber + 1;
-    console.log(caseNumber);
+   updateCaseNumber(true);
+});
+document.getElementById('case-minus').addEventListener('click',function(){
+    updateCaseNumber(false);
+
+
+// 
+
 })
